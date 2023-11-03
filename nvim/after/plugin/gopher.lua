a7513286ac3b689d37aa515b640cdffdda24cf14
Fijar Lazuardy <fijarlaz@gmail.com>
2023-11-03 22:15:34 +0700

    add go.nvim

--- nvim/after/plugin/gopher.lua
+++ nvim/after/plugin/gopher.lua
@@ -1,4 +1,14 @@
-require("go").setup()
+require("go").setup {
+    tag_options="",
+    test_runner='richgo',
+    run_in_floaterm=true,
+    floaterm = {   -- position
+        posititon = 'auto', -- one of {`top`, `bottom`, `left`, `right`, `center`, `auto`}
+        width = 0.45, -- width of float window if not auto
+        height = 0.98, -- height of float window if not auto
+        title_colors = 'dracula', -- default to nord, one of {'nord', 'tokyo', 'dracula', 'rainbow', 'solarized ', 'monokai'}
+  },
+}
 -- require("gopher").setup {
 --   commands = {
 --     go = "go",
@@ -26,3 +36,4 @@ vim.keymap.set("n", "<leader>gie", "<cmd> GoIfErr <CR>")
 vim.keymap.set("n", "<leader>gtj", "<cmd> GoAddTag json <CR>") 
 vim.keymap.set("n", "<leader>gtd", "<cmd> GoAddTag db <CR>")
 vim.keymap.set("n", "<leader>gfs", "<cmd> GoFillStruct <CR>")
+
